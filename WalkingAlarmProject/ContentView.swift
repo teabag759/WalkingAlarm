c1ab77a12a4cfe290ed8ec5600302e96cdf2f25a
soy <teabag759@gmail.com>
2025-02-27 20:50:19 +0900

    Initial commit

---
+++ WalkingAlarmProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalkingAlarmProject
+//
+//  Created by 이소현 on 2/27/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalkingAlarmProject/WalkingAlarmProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkingAlarmProjectApp.swift
+//  WalkingAlarmProject
+//
+//  Created by 이소현 on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct WalkingAlarmProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
